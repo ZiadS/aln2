0863d85ba5f8c681f4e4362734f4b19e2203f6e4
Ziad Giliana <ziadgiliana@gmail.com>
2015-09-25 14:29:50 +0200

    matrix fix

--- Spelling/src/ClosestWords.java
+++ Spelling/src/ClosestWords.java
@@ -1,6 +1,7 @@
 /* Labb 2 i DD1352 Algoritmer, datastrukturer och komplexitet    */
 /* Se labbanvisning under kurswebben https://www.kth.se/social/course/DD1352 */
 /* Ursprunglig författare: Viggo Kann KTH viggo@nada.kth.se      */
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,7 +11,10 @@ public class ClosestWords {
   int closestDistance = -1;
 
   int partDist(String w1, String w2, int w1len, int w2len) {
-	  
+	  if(checkMatrix(w1len, w2len)){	// If value exists
+		 // System.out.println(matrix[w1len][w2len]);
+		  return matrix[w1len][w2len];	 
+	  }
 	  if (w1len == 0)
       return w2len;
     if (w2len == 0)
@@ -22,18 +26,22 @@ public class ClosestWords {
     if (addLetter < res)
       res = addLetter;
     int deleteLetter = partDist(w1, w2, w1len, w2len - 1) + 1;
-    if (deleteLetter < res)
+    if (deleteLetter < res){
       res = deleteLetter;
+    }
+    matrix[w1len][w2len] = res;
     return res;
   }
 
   int Distance(String w1, String w2) {
-	  matrix = new int[w1.length()][w2.length()]; //initialize matrix with the right dimensions
+
+	  matrix = new int[w1.length() + 1][w2.length() + 1]; //initialize matrix with the right dimensions
 	    for(int i = 0; i < matrix.length; i++) {
 	    	for(int j = 0; j < matrix[i].length; j++) {
 	    		matrix[i][j] = -1;
 	    	}
 	    }
+	  // System.out.println(Arrays.deepToString(matrix));
     return partDist(w1, w2, w1.length(), w2.length());
   }
 
@@ -57,5 +65,8 @@ public class ClosestWords {
 
   List<String> getClosestWords() {
     return closestWords;
+  }
+  public boolean checkMatrix(int w1Index, int w2Index){
+	  return (matrix[w1Index][w2Index] != -1) ? true : false;
   }
 }
